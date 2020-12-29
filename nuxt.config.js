f1032b335983c834e54867ce45e994b5571fb865
Camillo Visini <c.visini@gmail.com>
2020-12-29 07:22:43 +0000

    Remove optional Nuxt folders

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,6 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
